a44b707013fa9b399d43af97bf29d978d04ddc21
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-07-10 00:29:12 -0700

    Create test_differential_solver.f90

---
+++ tests/test_differential_solver.f90
@@ -0,0 +1,7 @@
+program test_differential_solver
+    use differential_solver
+    implicit none
+
+    ! Test for differential equation solver to be added here
+
+end program test_differential_solver
