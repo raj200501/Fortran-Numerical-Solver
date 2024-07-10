cc89c60dc65660b14ae3e5db35b9467337b61524
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-07-10 00:27:00 -0700

    Create differential_solver.f90

---
+++ src/differential_solver.f90
@@ -0,0 +1,9 @@
+module differential_solver
+    implicit none
+    contains
+
+    subroutine solve_differential_equation()
+        ! Differential equation solver to be implemented here
+    end subroutine solve_differential_equation
+
+end module differential_solver
